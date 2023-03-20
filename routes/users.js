1de1efa1efa9949c0fab5cf52105a29dbacb98c2
John <jddhalogamer@gmail.com>
2023-03-20 16:41:33 -0500

    added middleware to verify jwt.

---
+++ routes/users.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const User = require('../models/User')
+const CryptoJS = require('crypto-js')
+const verify = require('../verifyToken')
+
+
+//Update
+
+router.put('/:id', async (req, res) => {
+
+})
+//Delete
+
+//Get
+
+//Get All Users
+
+//Get User Stats
