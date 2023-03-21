f334a44c5cbe5995907af359a874b64ba8b436ac
John <jddhalogamer@gmail.com>
2023-03-21 18:34:05 -0500

    updateMovie function added to movies controller.

--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,41 @@ const verify = require('../verifyToken')
 
 //Create
 
+router.post('/', verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    const newMovie = new Movie(req.body)
+
+    try {
+      const savedMovie = await newMovie.save()
+      res.status(201),json(savedMovie)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  } else {
+    res.status(403).json('You are not allowed!')
+  }
+})
+
 //Update
+
+router.put('/', verify, async (req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      const updatedMovie = await Movie.findByIdAndUpdate(
+        req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+      )
+        res.status(201),json(updatedMovie)
+      } catch (err) {
+        res.status(500).json(err)
+    }
+    } else {
+    res.status(403).json('You are not allowed!')
+  }
+})
 
 //Delete
 
